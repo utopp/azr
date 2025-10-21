4b2a1b4402fa073823f84e178212120fc827ccd0
utopp <uto.pampas@gmail.com>
2025-10-21 17:13:48 +0200

    add get token

--- R/defaults.R
+++ R/defaults.R
@@ -1,4 +1,83 @@
 
+default_azure_token <- function(scope =  NULL,
+                                tenant_id = NULL,
+                                client_id =  NULL,
+                                client_secret = NULL,
+                                use_cache = c("disk", "memory"),
+                                offline = FALSE,
+                                oauth_host = NULL,
+                                oauth_endpoint = NULL){
+
+  cli::cli_alert_info("Trying: Client Secret Credential")
+
+  creds <- try({
+    ClientSecretCredential$new(
+      scope = scope,
+      tenant_id = tenant_id,
+      client_id = client_id,
+      client_secret = client_secret,
+      oauth_host = oauth_host)
+  },
+  silent = TRUE)
+
+  if(inherits(creds, "Credential")){
+    token <- try(creds$get_token(), silent = TRUE)
+
+    if(inherits(token, "httr2_token")){
+      cli::cli_alert_success("Sucessful")
+      return(creds$get_token)
+    }
+  }
+
+  cli::cli_alert_danger("Unsucessful")
+
+  cli::cli_alert_info("Trying: Azure CLI Credential")
+
+  creds <- try({
+    AzureCLICredential$new(
+      scope = scope,
+      tenant_id = tenant_id)
+  },
+  silent = TRUE)
+
+  if(inherits(creds, "Credential")){
+    token <- try(creds$get_token(), silent = TRUE)
+
+    if(inherits(token, "httr2_token")){
+      return(creds$get_token)
+      cli::cli_alert_success("Sucessful")
+    }
+  }
+
+  cli::cli_alert_danger("Unsucessful")
+
+  if(!interactive()){
+    cli::cli_abort("Failed to find valid Credential for non-iteractive flows.")
+  }
+
+  cli::cli_alert_info("Trying: Interactive Flow")
+
+  creds <- try({
+    AzureCLICredential$new(
+      scope = scope,
+      tenant_id = tenant_id)
+  },
+  silent = TRUE)
+
+  if(inherits(creds, "Credential")){
+    token <- try(creds$get_token(), silent = TRUE)
+
+    if(inherits(token, "httr2_token")){
+      return(creds$get_token)
+      cli::cli_alert_success("Sucessful")
+    }
+  }
+  cli::cli_alert_danger("Unsucessful")
+
+  cli::cli_abort("Failed to find valid Credential.")
+}
+
+
 default_azure_tenant_id <- function() {
   Sys.getenv(EnvironmentVariables$AZURE_TENANT_ID,
              unset = AzureClient$TENANT_ID)
