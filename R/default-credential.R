c3e52ba14d1fd438f0ea5b99482d5de807448606
utopp <uto.pampas@gmail.com>
2025-10-23 09:49:32 +0200

    fix chain

--- R/default-credential.R
+++ R/default-credential.R
@@ -165,35 +165,40 @@ find_credential <- function(scope =  NULL,
                             verbose = FALSE){
   for(crd in chain){
 
-    if(R6::is.R6Class(crd))
+    if(R6::is.R6Class(crd)){
       obj <- try(new_instance(crd, env = rlang::current_env()), silent = TRUE)
-    else
-      obj <- crd
+      cli::cli_alert_info("Trying: {.cls {crd$classname}}")
 
-    if(obj$is_interactive() && !rlang::is_interactive()){
-      cli::cli_alert_warning("Skipping {.cls {class(obj)[[1]]}} (non-interactive session)")
-      next
-    } else {
+      if(!inherits(obj, "Credential")){
+        cli::cli_alert_danger("Unsuccessful!")
+        next
+      }
+    }
+    else {
+      obj <- crd
       cli::cli_alert_info("Trying: {.cls {class(obj)[[1]]}}")
     }
 
-    if(inherits(obj, "Credential")){
-      token <- tryCatch(obj$get_token(),
-                        error = function(e){
-                          if(isTRUE(verbose))
-                            print(e)
-                          else
-                            cli::cli_alert_danger("Unsuccessful!")
-                        }
-                        ,
-                        interrupt = function(e) {
-                          cli::cli_alert_danger("Interrupted!")
-                        })
+    if(obj$is_interactive() && !rlang::is_interactive()){
+      cli::cli_alert_warning("Skipping (non-interactive session)")
+      next
+    }
 
-      if(inherits(token, "httr2_token")){
-        cli::cli_alert_success("Sucessful!")
-        return(obj)
-      }
+    token <- tryCatch(obj$get_token(),
+                      error = function(e){
+                        if(isTRUE(verbose))
+                          print(e)
+                        else
+                          cli::cli_alert_danger("Unsuccessful!")
+                      }
+                      ,
+                      interrupt = function(e) {
+                        cli::cli_alert_danger("Interrupted!")
+                      })
+
+    if(inherits(token, "httr2_token")){
+      cli::cli_alert_success("Sucessful!")
+      return(obj)
     }
   }
   cli::cli_abort("All authentication methods of the chain failed!")
@@ -215,5 +220,5 @@ new_instance <- function(cls, env = rlang::caller_env()){
   cls_values <- rlang::env_get_list(nms = cls_args, default = NULL, env = env)
 
   eval(rlang::call2(cls$new, !!!cls_values))
-  }
+}
 
